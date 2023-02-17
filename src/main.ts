218a9e70aa41e96406587173039d8681ecb219d0
Gja <792002710@qq.com>
2023-02-17 21:17:41 +0800

    添加二维码

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,16 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('建议使用手机打开页面以保证浏览效果');
+  // 页面二维码
+  const img = document.createElement('img');
+  img.src = './qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
